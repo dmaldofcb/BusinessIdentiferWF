40690571cc585ddb4edd5d51ec4c59ebea61d021
Henri Espana <henriespana14@live.com>
2022-05-12 15:26:46 -0400

    initial DB config plus model

---
+++ InclusionDiversityIdentifier/Models/Business.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InclusionDiversityIdentifier.Models
+{
+    public class Business
+    {
+        [Key]
+        public int dunsNumId { get; set; }
+        public string dunsName { get; set; }
+        public string county { get; set; }
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string executiveContact { get; set; }
+        public string executiveContact2 { get; set; }
+        public bool isWomanOwned { get; set; }
+        public string minorityOwnedDesc { get; set; }
+    }
+}
