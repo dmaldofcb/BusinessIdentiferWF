359b12f6eca68473be666f5e444af049ebc58912
Henri Espana <henriespana14@live.com>
2022-05-12 15:39:38 -0400

    drop database

---
+++ InclusionDiversityIdentifier/Database/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using InclusionDiversityIdentifier.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InclusionDiversityIdentifier.Database
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):
+            base(options)
+        { 
+            
+        }
+        public DbSet<Business> Businesses { get; set; }
+    }
+}
