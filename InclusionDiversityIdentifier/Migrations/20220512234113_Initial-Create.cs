3f778f2a2b5135be0688b5c653211f246a8080f2
dmaldofcb <dmaldofcb@gmail.com>
2022-05-12 20:12:47 -0400

    database string setup

---
+++ InclusionDiversityIdentifier/Migrations/20220512234113_Initial-Create.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace InclusionDiversityIdentifier.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Businesses",
+                columns: table => new
+                {
+                    dunsNumId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    dunsName = table.Column<string>(type: "text", nullable: true),
+                    county = table.Column<string>(type: "text", nullable: true),
+                    streetAddress = table.Column<string>(type: "text", nullable: true),
+                    city = table.Column<string>(type: "text", nullable: true),
+                    state = table.Column<string>(type: "text", nullable: true),
+                    zipCode = table.Column<string>(type: "text", nullable: true),
+                    phoneNumber = table.Column<string>(type: "text", nullable: true),
+                    executiveContact = table.Column<string>(type: "text", nullable: true),
+                    executiveContact2 = table.Column<string>(type: "text", nullable: true),
+                    isWomanOwned = table.Column<bool>(type: "boolean", nullable: false),
+                    minorityOwnedDesc = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Businesses", x => x.dunsNumId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Businesses");
+        }
+    }
+}
